4527161a2c8b842f8b74c0befea25f6e01aec01c
weitalu <wei-ta.lu@logicsolutions.com>
2016-08-20 19:27:22 +0800

    fix typo

--- renderer.js
+++ renderer.js
@@ -2,7 +2,6 @@
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
 const {clipboard} = require('electron')
-
 var answerAsync = require('./answerService.js')
 
 module.exports = {
