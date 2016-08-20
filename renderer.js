dd161c0a48c37ba05d22df6b68f32047fe6a3389
weitalu <wei-ta.lu@logicsolutions.com>
2016-08-20 12:20:09 +0800

    add example code

--- renderer.js
+++ renderer.js
@@ -1,3 +1,10 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+const {clipboard} = require('electron')
+alert(clipboard.readText("String"))
+module.exports = function demoClipboard()
+{
+    var text = clipboard.readText("String");
+    alert(text);
+}
