f4c8ea120c6c8ce9fb51cc148edafb07b27d7395
Koch Lo <bandangt@gmail.com>
2016-08-20 21:30:58 +0800

    Enhance dynamic content fill api

--- modalService.js
+++ modalService.js
@@ -6,8 +6,16 @@ var ModalService = function(selector) {
 
 ModalService.prototype = _.assignIn(ModalService.prototype, {
   show: function(content) {
-		this.$modal.find('.modal-body').text(content);
-		this.$modal.modal('show');
+    if (_.isString(content)) {
+      this.$modal.find('.modal-body').text(content);
+    }
+
+    if (_.isObject(content)) {
+      this.$modal.find('.modal-title').text(content.title);
+      this.$modal.find('.modal-body').text(content.body);
+    }
+
+    this.$modal.modal('show');
   },
   hide: function() {
   	this.$modal.modal('hide');
